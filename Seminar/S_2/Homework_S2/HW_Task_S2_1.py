e372b5630218e18193c8cb0442b9a7fbd6a0eced
Kirill <kirill.uvarov@gmail.com>
2022-11-27 20:37:19 +0300

    Seminar_2_2

---
+++ Seminar/S_2/Homework_S2/HW_Task_S2_1.py
@@ -0,0 +1,25 @@
+# 1 - Напишите программу, которая принимает на вход вещественное 
+# число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
+# Пример:
+# 67.82 -> 23
+# (-0.56) -> 11
+
+number = list (input('Введите вещественное число: '))
+# for (number%10 = 0):
+#     a = number%10
+print(number%10)
+print(number//10)
+
+# print(number[2:])
+# print(word[:2])
+# print(word[1:3])
+# # thon
+# # py
+# # yt
+
+
+# sum = number%10
+
+# for i in range(0, number):
+#     i+=1
+#     print(number)
