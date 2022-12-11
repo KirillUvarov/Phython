ccb404c3cd1c75471ec8a70e77049c77fcda6517
Kirill <kirill.uvarov@gmail.com>
2022-12-11 14:14:43 +0300

    Lectures_4

--- Lectures/Lecture_2/Example_4_cortege.py
+++ Lectures/Lecture_2/Example_4_cortege.py
@@ -1,12 +1,12 @@
 # Кортеж - неизменяемый список. 
 # присвоить новое значение его элементам не получится
 
-a = (3, 1, 5, 4)
+# a = (3, 1, 5, 4)
 # print (a)
 # print(a[-2])
 
-for item in a:
-    print(item)
+# for item in a:
+#     print(item)
 
 # Кортежи
 # t = tuple(['red', 'green', 'blue'])
@@ -16,11 +16,11 @@ for item in a:
 # print(t[-2]) # green
 # # print(t[-200]) # IndexError: tuple index out of range
 # for e in t:
-# print(e) # red green blue
+#     print(e) # red green blue
 # t[0] = 'black' # TypeError: 'tuple' object does not support
-# item assignment
+#     # item assignment
 
 # = tuple(['red', 'green', 'blue'])
 # red, green, blue = t
 # print('r:{} g:{} b:{}'.format(red, green, blue))
-# # r:red g:green b:blue
+# r:red g:green b:blue
---
+++ Lectures/Lecture_4/Example_1.py
@@ -0,0 +1,13 @@
+# print(( (x =(input('1'))) + (y=(input('2')))))
+x = 0
+y = 0
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+def sum():
+    return x + y
+
+de
